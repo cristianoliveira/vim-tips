7bca17a0a3bf9f6b35988c6aafecb28244fe6cd2
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-03-01 22:52:44 +0100

    chore(infra): port heroku specific cmds

--- Makefile
+++
@@ -1,7 +0,0 @@
-.PHONY: deploy watch-logs
-
-deploy:
-	git push heroku main
-
-watch-logs:
-	heroku logs --tails
--- scripts/post-twitter.sh
+++ scripts/post-twitter.sh
@@ -1,3 +1,3 @@
-curl -XPOST https://vim-tips.herokuapp.com/twitter \
+curl -XPOST $SERVICE_API_URL/twitter \
   -H "Content-Type: application/json" \
   -d '{"secret": "'"$VIM_TIPS_API_SECRET"'"}'
